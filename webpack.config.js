15452e35f425fd17c392b64eb636233015360302
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-04-14 17:42:08 +0900

    :sparkles: Introduce typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { resolve, path } = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/ts/sketch.ts",
+  output: {
+    filename: "[name].bundle.js",
+    path: resolve("dist"),
+    publicPath: "/public/"
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts|.tsx$/,
+        use: {
+          loader: "ts-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    host: "localhost",
+    port: 9000,
+    contentBase: resolve("static")
+  },
+  plugins: [new webpack.NoEmitOnErrorsPlugin()]
+};
